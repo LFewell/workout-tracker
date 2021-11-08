bab85c7bf003029605e1b5eade98484da5138660
Lincoln Fewell <LincolnTFewell@gmail.com>
2021-11-07 20:11:53 -0500

    Set up the server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+app.listen(PORT, () => {
+    console.log(`App is listening on http://localhost:${PORT}`);
+});
