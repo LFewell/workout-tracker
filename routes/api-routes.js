66404d12569debf64e4d6e78d54ca85eec0da739
Lincoln Fewell <LincolnTFewell@gmail.com>
2021-11-10 00:38:32 -0500

    Finished routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -20,4 +20,50 @@ router.get("/api/workouts", (req, res) => {
     })
 });
 
+router.get("/api/workouts/range", async (req, res) => {
+    Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: {
+                    $sum: "$exercises.duration",
+                }
+            }
+        }
+    ])
+    .then(dbWorkout => {
+        res.json(dbWorkout)
+    })
+    .catch(err => {
+        res.status(400).json(err)
+    })
+});
+
+router.put("/api/workouts/:id", async (req, res) => {
+    Workout.findByIdAndUpdate(req.params.id,
+        {
+            $push: { exercises: req.body }
+        },
+        {
+            new: true
+        },
+        )
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+router.post("/api/workouts", async (req,res) => {
+    Workout.create(req.body)
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    })
+});
+
+
 module.exports = router;
