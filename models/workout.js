6e196f10390f9f033d55d94b7c79117f58bc8a67
Lincoln Fewell <LincolnTFewell@gmail.com>
2021-11-09 21:34:42 -0500

    Added models

---
+++ models/workout.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now()
+    },
+    exercises: [
+        {
+            type: {
+                type: String,
+                trim: true,
+                required: "An exercise type must be selected"
+            },
+            name: {
+                type: String,
+                trim: true,
+                required: "Please enter an exercise name"
+            },
+            distance: {
+                type: Number
+            },
+            duration: {
+                type: Number
+            },
+            weight: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            }   
+        }
+    ]
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
