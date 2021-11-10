f23c2a8eb9cbdad1f0c8c6ccbe0d179b90746b06
Lincoln Fewell <LincolnTFewell@gmail.com>
2021-11-09 20:56:53 -0500

    Created routes for the html pages

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
@@ -8,6 +9,8 @@ app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
 app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/")
 
 app.listen(PORT, () => {
     console.log(`App is listening on http://localhost:${PORT}`);
